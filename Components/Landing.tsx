d3338a4b989dba84d70897a8e289b2e4dac41885
Mustak9009 <91077142+Mustak9009@users.noreply.github.com>
2022-12-12 00:12:12 +0530

    add landing page more elements and second page 12-12-20222 :12:12am

--- Components/Landing.tsx
+++ Components/Landing.tsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import Image from 'next/image';
+import Button from './Button';
 export default function Landing() {
   return (
-    <section className='sticky top-0 h-screen flex justify-between  items-center px-8 mx-auto max-w-[1350px]'>
+    <section className='sticky top-0 h-screen flex justify-between  items-center px-8 mx-auto max-w-[1450px] '>
         <div className='space-y-8'>
             <h1 className='font-semibold space-y-3 text-5xl lg:text-6xl xl:text-7xl'>
                 <span className='block bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent'>Powered</span> {/*bg-clip-text text-transparent => Help to provide text -> color effect*/}
                 <span className='block'>By Intellect</span>
                 <span>Driven By Values</span>
             </h1>
-            <div>
+            <div className='space-x-9'>
+              <Button title={'Buy me'}/>
               <a className='link'>Learn more</a>
             </div>
             
         </div>
         <div className='relative hidden w-[450px] h-[450px] transition-all duration-500 md:inline lg:h-[650px] lg:w-[650px]'>
-              <img src='/apple.webp' alt={'apple 13'}/>
+              <Image src='/apple.png' alt={'apple 13'} width={500} height={500}/>
             </div>
     </section>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,10 +11,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
-      <main>
+      <main className='relative h-[200vh] bg-[#E7ECEE]'>
         <Landing/>
-        
       </main>
+      <section className='relative z-40 -mt-[100vh] min-h-screen  bg-[#1b1b1b]'>
+        <div className='space-y-10 py-16'>
+        <h1 className='text-center text-4xl text-white tracking-wide font-medium md:text-5xl'>New promos</h1>
+        </div>
+      </section>
     </div>
   )
 }
